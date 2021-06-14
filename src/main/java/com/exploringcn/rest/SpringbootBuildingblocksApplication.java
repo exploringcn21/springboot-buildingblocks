dd17ec862f442e81cfb33e9fd5bcbc259b665828
tonmoy-at-433107525758 <exploringcn21@gmail.com>
2021-06-14 23:10:04 +0100

    second commit

--- src/main/java/com/exploringcn/rest/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/exploringcn/rest/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,8 @@ package com.exploringcn.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// this is a test app
+
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
